5b4be24c04d1984075d106f7307f3ecf8ac819e0
Jerry Cherian <jerryputh2000@gmail.com>
2020-07-23 15:31:13 -0400

    Initial Commit with Basic Login Functionality

--- API/APIserver.py
+++ API/APIserver.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for, redirect, request
 
 app = Flask(__name__, static_url_path='')
+logged_in = False
 
 @app.route('/', methods=['GET'])
 def home():
     return render_template('welcomepage.html')
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        user = request.form.get('username')
+        password = request.form.get('password')
+        if user == "admin" and password == "123456":
+            logged_in = True
+            return render_template('moodpage.html')
+        else:
+            return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
