42120e6b03356e019cf7b5530364185e9b6c1f93
Jerry Cherian <jerryputh2000@gmail.com>
2020-07-23 17:44:37 -0400

    Mood Functionality with Logout Functionality

--- API/APIserver.py
+++ API/APIserver.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template, url_for, redirect, request
+from datetime import date, timedelta, datetime
 
 app = Flask(__name__, static_url_path='')
+
+today = date.today()
+yesterday = today - timedelta(days = 1)
+previousLoginDate = False
+
+#Should be authenticated better
 logged_in = False
+
+#Should be stored in a database so it can be retrieved
+streak = 1
 
 @app.route('/', methods=['GET'])
 def home():
@@ -10,9 +20,11 @@ def home():
 
 @app.route('/login', methods=['GET','POST'])
 def login():
+    global logged_in
     if request.method == 'GET':
         return render_template('login.html')
     if request.method == 'POST':
+        #Should be retrieved from database, password should be hashed.
         user = request.form.get('username')
         password = request.form.get('password')
         if user == "admin" and password == "123456":
@@ -20,6 +32,28 @@ def login():
             return render_template('moodpage.html')
         else:
             return redirect(url_for('login'))
+
+@app.route('/mood', methods=['GET', 'POST'])
+def mood():
+    global streak
+    global previousLoginDate
+    if request.method == 'GET':
+        if logged_in:
+            return render_template("moodpage.html")
+        else:
+            return redirect(url_for('login'))
+    if request.method == 'POST':
+            if previousLoginDate == yesterday:
+                streak += 1
+            previousLoginDate = today
+            return render_template("ratingpage.html", rating = request.form.get('rating'), streak=streak)
+
+@app.route('/logout')
+def logout():
+    global logged_in
+    logged_in = False
+    return render_template('welcomepage.html')
+
 
 
 if __name__ == '__main__':
